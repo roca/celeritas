5c3c65ff097c37297658f4e3392ccde817758f28
Romel Campbell <RomelCampbell@gmail.com>
2023-05-23 03:50:27 -0400

    WIP: Renderer package

---
+++ render/render.go
@@ -0,0 +1,27 @@
+package render
+
+import "net/http"
+
+type Render struct {
+	Renderer   string
+	RootPath   string
+	Secure     bool
+	Port       string
+	ServerName string
+}
+
+type TemplateData struct {
+	IsAuthenticated bool
+	IntMap          map[string]int
+	StringMap       map[string]string
+	FloatMap        map[string]float32
+	Data            map[string]interface{}
+	CSRFToken       string
+	Port            string
+	ServerName      string
+	Secure          bool
+}
+
+func (c *Render) Page(w http.ResponseWriter, r *http.Request, view string, variables, data interface{}) error {
+	return nil
+}
