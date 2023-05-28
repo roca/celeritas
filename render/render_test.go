247cc1951230bf323ee2496b069ef995951bde64
Romel Campbell <RomelCampbell@gmail.com>
2023-05-28 04:32:54 -0400

    Refactored test to table pattern

--- render/render_test.go
+++ render/render_test.go
@@ -3,49 +3,46 @@ package render
 import (
 	"net/http"
 	"net/http/httptest"
-	"strings"
 	"testing"
 )
 
+var pageData = []struct {
+	name          string
+	renderer      string
+	template      string
+	errorExpected bool
+	errorMessage  string
+}{
+	{"go_page", "go", "home", false, "error rendering go template"},
+	{"go_page_no_template", "go", "no-file", true, "no error rendering non-existent template, when one is expected"},
+	{"jet_page", "jet", "home", false, "error rendering jet template"},
+	{"jet_page_no_template", "jet", "no-file", true, "no error rendering non-existent template, when one is expected"},
+	{"invalid_render_engine", "foo", "home", true, "no error rendering with non-existent template engine"},
+}
+
 func TestRender_Page(t *testing.T) {
-	r, err := http.NewRequest("GET", "/some-url", nil)
-	if err != nil {
-		t.Error(err)
-	}
-	w := httptest.NewRecorder()
+	for _, tt := range pageData {
+		t.Run(tt.name, func(t *testing.T) {
+			r, err := http.NewRequest("GET", "/some-url", nil)
+			if err != nil {
+				t.Error(err)
+			}
+			w := httptest.NewRecorder()
 
-	testRender.Renderer = "go"
-	testRender.RootPath = "./testdata"
+			testRender.Renderer = tt.renderer
+			testRender.RootPath = "./testdata"
 
-	err = testRender.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
-
-	if strings.Contains(w.Body.String(), "Hello World") == false {
-		t.Error("Body does not match expected output")
-	}
-
-	err = testRender.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Rendered template that does not exist")
-	}
-
-	testRender.Renderer = "jet"
-	err = testRender.Page(w, r, "home", nil, nil)
-	if err != nil {
-		t.Error("Error rendering page", err)
-	}
-
-	err = testRender.Page(w, r, "no-file", nil, nil)
-	if err == nil {
-		t.Error("Rendered Jet template that does not exist")
-	}
-
-	testRender.Renderer = "unknown"
-	err = testRender.Page(w, r, "home", nil, nil)
-	if err == nil {
-		t.Error("No error return while rendering with unknown renderer")
+			err = testRender.Page(w, r, tt.template, nil, nil)
+			if tt.errorExpected {
+				if err == nil {
+					t.Errorf("%s: %s", tt.name, tt.errorMessage)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("%s: %s: %s", tt.name, tt.errorMessage, err.Error())
+				}
+			}
+		})
 	}
 }
 
