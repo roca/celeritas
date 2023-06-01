8cee46268db7a3cd89df9708b36fe043a681cad1
Romel Campbell <RomelCampbell@gmail.com>
2023-06-01 03:48:45 -0400

    adds session middleware

--- celeritas.go
+++ celeritas.go
@@ -86,6 +86,7 @@ func (c *Celeritas) New(rootPath string) error {
 			lifetime: os.Getenv("COOKIE_LIFETIME"),
 			persist:  os.Getenv("COOKIE_PERSISTS"),
 			secure:   os.Getenv("COOKIE_SECURE"),
+			domain:   os.Getenv("COOKIE_DOMAIN"),
 		},
 		sessionType: os.Getenv("SESSION_TYPE"),
 	}
@@ -96,6 +97,7 @@ func (c *Celeritas) New(rootPath string) error {
 		CookiePersist:  c.config.cookie.persist,
 		CookieName:     c.config.cookie.name,
 		SessionType:    c.config.sessionType,
+		CookieDomain:   c.config.cookie.domain,
 	}
 	c.Session = sess.InitSession()
 
