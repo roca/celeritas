4d435b34963243a40fe0f6c4cc8b1691dc1849fc
Romel Campbell <RomelCampbell@gmail.com>
2023-06-02 05:15:45 -0400

    Session middleware confirmed

--- middleware.go
+++ middleware.go
@@ -3,5 +3,6 @@ package celeritas
 import "net/http"
 
 func (c *Celeritas) SessionLoad(next http.Handler) http.Handler {
+	c.InfoLog.Println("SessionLoad middleware called")
 	return c.Session.LoadAndSave(next)
 }
