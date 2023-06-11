94549fb5cad39da3c1fd8c5324491a91f341e9ad
Romel Campbell <RomelCampbell@gmail.com>
2023-06-11 04:29:59 -0400

    Random String helper func

--- helpers.go
+++ helpers.go
@@ -1,11 +1,31 @@
 package celeritas
 
-import "os"
+import (
+	"crypto/rand"
+	"os"
+)
+
+const (
+	randomString = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_+"
+)
+
+// RandomString generates a random string of length n from values in the const randomString
+func (c *Celeritas) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomString)
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x, y := p.Uint64(), uint64(len(r))
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
 
 func (c *Celeritas) CreateDirIfNotExist(path string) error {
 	// create folder if it doesn't exist
 	const mode = 0755
-	if _,err := os.Stat(path); os.IsNotExist(err) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err := os.Mkdir(path, mode)
 		if err != nil {
 			return err
@@ -17,7 +37,7 @@ func (c *Celeritas) CreateDirIfNotExist(path string) error {
 func (c *Celeritas) CreateFileIfNotExist(path string) error {
 	// create folder if it doesn't exist
 	const mode = 0644
-	if _,err := os.Stat(path); os.IsNotExist(err) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		file, err := os.Create(path)
 		if err != nil {
 			return err
