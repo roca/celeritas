e1036fdfb9e6ba016e6ad5c3cf1d8ab99c8e7d8e
romel.campbell <romel.campbell@regeneron.com>
2023-07-20 16:01:04 -0400

    WIP

--- cmd/cli/helpers.go
+++ cmd/cli/helpers.go
@@ -26,13 +26,13 @@ func getDSN() string {
 	dbType := cel.DB.DataType
 
 	if dbType == "pgx" {
-		dbType = "postgress"
+		dbType = "postgres"
 	}
 
 	if dbType == "postgres" {
 		var dsn string
 		if os.Getenv("DATABASE_PASS") != "" {
-			dsn = fmt.Sprintf("postgress://%s:%s@%s:%s/%s?sslmode%s",
+			dsn = fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s",
 				os.Getenv("DATABASE_USER"),
 				os.Getenv("DATABASE_PASS"),
 				os.Getenv("DATABASE_HOST"),
@@ -41,7 +41,7 @@ func getDSN() string {
 				os.Getenv("DATABASE_SSL_MODE"),
 			)
 		} else {
-			dsn = fmt.Sprintf("postgress://%s@%s:%s/%s?sslmode%s",
+			dsn = fmt.Sprintf("postgres://%s@%s:%s/%s?sslmode=%s",
 				os.Getenv("DATABASE_USER"),
 				os.Getenv("DATABASE_HOST"),
 				os.Getenv("DATABASE_PORT"),
