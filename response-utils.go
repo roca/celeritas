6f6a956ed41a880d5f465ddfcd5f4935db9dec0c
Romel Campbell <RomelCampbell@gmail.com>
2023-07-26 16:54:34 -0400

    WriteJson

---
+++ response-utils.go
@@ -0,0 +1,27 @@
+package celertias
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (c *Celertias) WriteJSON(w http.ResponseWriter, status int, payload interface{}, headers ...http.Header) error {
+	out,err :=  json.MarshalIndent(payload, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_,err = w.Write(w, out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
