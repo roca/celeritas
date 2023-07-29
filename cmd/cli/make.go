6b6357f3adb5e6e29866c32765cd05b9c52f649b
Romel Campbell <RomelCampbell@gmail.com>
2023-07-29 04:16:08 -0400

    WIP: Code boken

--- cmd/cli/make.go
+++ cmd/cli/make.go
@@ -5,7 +5,9 @@ import (
 	"io/ioutil"
 	"strings"
 
+	"github.com/gertd/go-pluralize"
 	"github.com/iancoleman/strcase"
+	"k8s.io/kube-openapi/cmd/openapi-gen/args"
 )
 
 func doMake(arg2, arg3 string) error {
@@ -44,6 +46,34 @@ func doMake(arg2, arg3 string) error {
 		if err != nil {
 			exitGracefully(err)
 		}
+
+	case "model":
+		if arg3 == "" {
+			exitGracefully(errors.New("you must give the model a name"))
+		}
+
+		data, err := templateFS.ReadFile("templates/data/model.go.txt")
+		if err != nil {
+			exitGracefully(err)
+		}
+
+		model := string(data)
+
+		plur := pluralize.NewClient()
+
+		var modelName = arg3
+		var tableName = arg3
+
+		if plur.IsPlural(arg3) {
+			modelName = plur.Singular(arg3)
+			tableName = strings.ToLower(tableName)
+		} else {
+			tableName = strings.ToLower(plur.Plural(arg3))
+		}
+
+		fileName := cel.RooPath + "/data/" + strings.ToLower(modelName) + ".go"
+
+
 	default:
 		return errors.New("make requires a subcommand: (migration|model|handler)")
 	}
