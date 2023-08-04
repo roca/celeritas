c5d52be1b1a2cda2d990d307c31996ea2781c016
Romel Campbell <RomelCampbell@gmail.com>
2023-08-04 11:17:08 -0400

    func for some common error responses

--- response-utils.go
+++ response-utils.go
@@ -58,3 +58,32 @@ func (c *Celeritas) DownloadFile(w http.ResponseWriter, r *http.Request, pathToF
 	http.ServeFile(w, r, fileToServer)
 	return nil
 }
+
+// 404 NotFound Error
+func (c *Celeritas) Error404(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusNotFound)
+}
+
+// 500 InternalServer Error
+func (c *Celeritas) Error500(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusInternalServerError)
+}
+
+// 400 BadRequest Error
+func (c *Celeritas) Error400(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusBadRequest)
+}
+
+// 401 Unauthorized Error
+func (c *Celeritas) ErrorUnauthorized(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusUnauthorized)
+}
+
+// 403 Forbidden Error
+func (c *Celeritas) ErrorForbidden(w http.ResponseWriter, r *http.Request) {
+	c.ErrorStatus(w, http.StatusForbidden)
+}
+
+func (c *Celeritas) ErrorStatus(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
