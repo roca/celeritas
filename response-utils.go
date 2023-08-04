8f79c1c9b09e20614f0dbd0eead941546d8a0a66
Romel Campbell <RomelCampbell@gmail.com>
2023-08-04 05:13:15 -0400

    WIP: DownloadFile

--- response-utils.go
+++ response-utils.go
@@ -2,6 +2,7 @@ package celeritas
 
 import (
 	"encoding/json"
+	"encoding/xml"
 	"net/http"
 )
 
@@ -23,5 +24,31 @@ func (c *Celeritas) WriteJSON(w http.ResponseWriter, status int, payload interfa
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (c *Celeritas) WriteJXML(w http.ResponseWriter, status int, payload interface{}, headers ...http.Header) error {
+	out, err := xml.MarshalIndent(payload, "", "   ")
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for k, v := range headers[0] {
+			w.Header()[k] = v
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Celeritas) DownloadFile(w http.ResponseWriter, r *http.Request, filename string) error {
+	http.ServeFile(w, r, filename)
 	return nil
 }
