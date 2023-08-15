75d634a02d0d5f446566c8ffc4761ba57470a4e3
romel.campbell <romel.campbell@regeneron.com>
2023-08-15 15:30:31 -0400

    Toggle for Development mode

--- celeritas.go
+++ celeritas.go
@@ -147,12 +147,20 @@ func (c *Celeritas) New(rootPath string) error {
 	c.Session = sess.InitSession()
 	c.EncryptionKey = os.Getenv("KEY")
 
-	var views = jet.NewSet(
-		jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
-		jet.InDevelopmentMode(),
-	)
+	if c.Debug {
+		var views = jet.NewSet(
+			jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
+			jet.InDevelopmentMode(),
+		)
 
-	c.JetViews = views
+		c.JetViews = views
+	} else {
+		var views = jet.NewSet(
+			jet.NewOSFileSystemLoader(fmt.Sprintf("%s/views", rootPath)),
+		)
+
+		c.JetViews = views
+	}
 
 	c.createRenderer()
 
