e7655b77ca116d8005ebdc1d5434a38e5acc6805
Romel Campbell <RomelCampbell@gmail.com>
2023-09-04 12:27:35 -0400

    make auth includes forgot/reset password features

--- cmd/cli/auth.go
+++ cmd/cli/auth.go
@@ -26,6 +26,11 @@ func doAuth() error {
 		exitGracefully(err)
 	}
 
+	err = copyFileFromTemplate("templates/data/remember_token.go.txt", cel.RooPath+"/data/remember_token.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
 	// copy over middleware
 	err = copyFileFromTemplate("templates/middleware/auth.go.txt", cel.RooPath+"/middleware/auth.go")
 	if err != nil {
@@ -36,6 +41,42 @@ func doAuth() error {
 	if err != nil {
 		exitGracefully(err)
 	}
+
+	err = copyFileFromTemplate("templates/middleware/remember.go.txt", cel.RooPath+"/middleware/remember.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/handlers/auth-handlers.go.txt", cel.RooPath+"/handlers/auth-handlers.go")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/mailer/password-reset.html.tmpl", cel.RooPath+"/mail/password-reset.html.tmpl")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/mailer/password-reset.plain.tmpl", cel.RooPath+"/mail/password-reset.plain.tmpl")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/login.jet", cel.RooPath+"/views/login.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+	
+	err = copyFileFromTemplate("templates/views/forgot.jet", cel.RooPath+"/views/forgot.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	err = copyFileFromTemplate("templates/views/reset-password.jet", cel.RooPath+"/views/reset-password.jet")
+	if err != nil {
+		exitGracefully(err)
+	}
+
 
 	color.Yellow(" - users, tokens, and remember_tokens migrations created and executed")
 	color.Yellow(" - user and token models created")
