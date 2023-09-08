d20f15d4a5639e2c3cbbfdcd945082a9584697f0
Romel Campbell <RomelCampbell@gmail.com>
2023-09-08 05:16:38 -0400

    new creates go.mod and Makefile

--- cmd/cli/new.go
+++ cmd/cli/new.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"runtime"
 	"strings"
 
 	"github.com/fatih/color"
 	"github.com/go-git/go-git/v5"
 )
 
+var appURL string
+
 func doNew(appName string) {
 	appName = strings.ToLower(appName)
+	appURL = appName
 
 	// sanitize the application name (convert url to single word)
 	if strings.Contains(appName, "/") {
@@ -67,8 +72,62 @@ func doNew(appName string) {
 	}
 
 	// create a makefile
+	if runtime.GOOS == "windows" {
+		source, err := os.Open(fmt.Sprintf("./%s/Makefile.windows", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer source.Close()
+
+		destination, err := os.Create(fmt.Sprintf("./%s/Makefile", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer destination.Close()
+
+		_, err = io.Copy(destination, source)
+		if err != nil {
+			exitGracefully(err)
+		}
+
+	} else {
+		source, err := os.Open(fmt.Sprintf("./%s/Makefile.mac", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer source.Close()
+
+		destination, err := os.Create(fmt.Sprintf("./%s/Makefile", appName))
+		if err != nil {
+			exitGracefully(err)
+		}
+		defer destination.Close()
+
+		_, err = io.Copy(destination, source)
+		if err != nil {
+			exitGracefully(err)
+		}
+	}
+
+	_ = os.Remove("./" + appName + "/Makefile.mac")
+	_ = os.Remove("./" + appName + "/Makefile.windows")
 
 	// update the go.mod file
+	color.Yellow("\tCreating go.mod file...")
+	_ = os.Remove("./" + appName + "/go.mod")
+
+	data, err = templateFS.ReadFile("templates/go.mod.txt")
+	if err != nil {
+		exitGracefully(err)
+	}
+
+	mod := string(data)
+	mod = strings.ReplaceAll(mod, "${APP_NAME}", appURL)
+
+	err = copyDataToFile([]byte(mod), fmt.Sprintf("./%s/go.mod", appName))
+	if err != nil {
+		exitGracefully(err)
+	}
 
 	// update existing .go files with correct name/imports
 
